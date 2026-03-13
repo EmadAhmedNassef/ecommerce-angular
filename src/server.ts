e4662ed6615ef3452a2c12f9261f2292a88fc0a8
EmadAhmedNasef <emad_EN30895165@f-eng.tanta.edu.eg>
2026-03-13 21:54:15 +0200

    - added some files for vercel

--- src/server.ts
+++ src/server.ts
@@ -41,9 +41,7 @@ app.use(
 app.use((req, res, next) => {
   angularApp
     .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
+    .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
     .catch(next);
 });
 
@@ -51,16 +49,16 @@ app.use((req, res, next) => {
  * Start the server if this module is the main entry point, or it is ran via PM2.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
-if (isMainModule(import.meta.url) || process.env['pm_id']) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
-    if (error) {
-      throw error;
-    }
+// if (isMainModule(import.meta.url) || process.env['pm_id']) {
+//   const port = process.env['PORT'] || 4000;
+//   app.listen(port, (error) => {
+//     if (error) {
+//       throw error;
+//     }
 
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
-}
+//     console.log(`Node Express server listening on http://localhost:${port}`);
+//   });
+// }
 
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
