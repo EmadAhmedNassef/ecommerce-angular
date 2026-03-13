9625adac5ea0be3b7d56c1d6d15f3a438380b549
EmadAhmedNasef <emad_EN30895165@f-eng.tanta.edu.eg>
2026-03-13 20:59:08 +0200

    - added hash location

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,10 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
+import {
+  provideRouter,
+  withHashLocation,
+  withInMemoryScrolling,
+  withViewTransitions,
+} from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
@@ -15,6 +20,7 @@ export const appConfig: ApplicationConfig = {
       routes,
       withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'top' }),
       withViewTransitions(),
+      withHashLocation(),
     ),
     provideClientHydration(withEventReplay()),
     provideTanStackQuery(
